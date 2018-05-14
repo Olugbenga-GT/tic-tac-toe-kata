1a014001067997ece18fdeabf4ed6a70352a5184
eparlato <eparlato82@gmail.com>
2018-05-14 23:41:15 +0200

    First step toward a 3x3 grid

--- src/main/java/it/eparlato/tictactoe/Board.java
+++ src/main/java/it/eparlato/tictactoe/Board.java
@@ -4,24 +4,67 @@ import java.io.PrintStream;
 
 public class Board {
 
-	private String content = " ";
+	private String[][] content = new String[][] {
+			{" "}
+	};
 	private PrintStream output;
+	private int tot_rows;
+	private int tot_columns;
 
 	public Board(PrintStream output) {
+		this(output, 1, 1);
+	}
+
+	public Board(PrintStream output, int tot_rows, int tot_columns) {
 		this.output = output;
+		this.tot_rows = tot_rows;
+		this.tot_columns = tot_columns;
+		
+		content = new String[tot_rows][tot_columns];
+		
+		for (int i=0; i<tot_rows; i++) {
+			for (int j=0; j<tot_columns; j++) {
+				content[i][j] = " ";
+			}
+		}
 	}
 
 	public void takeField(String field) {
-		content = "X";
-	}
-
-	public String getContent() {
-		return content;
+		content[0][0] = "X";
 	}
 
 	public void print() {
-		output.print("  A\n");
-		output.print("1 " + content + "\n");
+		
+		// TODO: please refactor me
+		
+		String header = "";
+		
+		if(content[0].length == 1) {
+			header = "  A\n";
+		}
+		if (content[0].length == 3) {
+			header = "  A B C\n";
+		}
+		
+		output.print(header);
+		
+		StringBuilder row;
+		for (int i=0; i<tot_rows; i++) {
+			row = new StringBuilder();
+			
+			row.append(i+1).append(" ");
+			
+			for(int j=0; j<tot_columns; j++) {
+				row.append(content[i][j]);
+				
+				if(j < tot_columns - 1) {
+					row.append("|");
+				}
+			}
+			row.append("\n");
+			
+			output.print(row.toString());
+		}
 	}
 
 }
---
+++ src/test/java/it/eparlato/tictactoe/BoardTest.java
@@ -0,0 +1,61 @@
+package it.eparlato.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class BoardTest {
+	
+	private ByteArrayOutputStream outputBaos;
+	private PrintStream printStream;
+	private Board board;
+	
+	@Before
+	public void init() {
+		outputBaos = new ByteArrayOutputStream();
+		printStream = new PrintStream(outputBaos);
+		
+	}
+	
+	@After
+	public void shutdown() throws Exception {
+		printStream.close();
+		outputBaos.close();
+	}
+	
+	@Test
+	public void empty_1x1_board() throws Exception {
+		board = new Board(printStream);
+		
+		String expected = 
+				"  A\n" +
+				"1  \n";
+		
+		board.print();
+		
+		assertEquals(expected, outputBaos.toString("UTF-8"));
+	}
+	
+	@Test
+	public void empty_3x3_board() throws Exception {
+		board = new Board(printStream, 3, 3);
+		
+		String expected = 
+				"  A B C\n" +
+				"1  | | \n" +
+				"2  | | \n"	+	
+				"3  | | \n"
+				;
+		
+		
+		board.print();
+		
+		assertEquals(expected, outputBaos.toString("UTF-8"));
+	}
+}

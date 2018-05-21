9fe54f25abef1743c973cc2af6b6a08499a65a2e
eparlato <eparlato82@gmail.com>
2018-05-21 22:09:15 +0200

    Application test moved in proper test class

---
+++ src/test/java/it/eparlato/tictactoe/ApplicationTest.java
@@ -0,0 +1,55 @@
+package it.eparlato.tictactoe;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApplicationTest {
+	ByteArrayOutputStream outputBaos;
+	PrintStream printStream;
+	
+	@Before
+	public void init() {
+		outputBaos = new ByteArrayOutputStream();
+		printStream = new PrintStream(outputBaos);
+	}
+	
+	@After
+	public void shudtdown() throws IOException {
+		printStream.close();
+		outputBaos.close();
+	}
+	
+	@Test
+	public void application_accepts_input_from_a_inputstream() throws Exception {
+
+		
+		
+		String command = "A1\n";
+		Reader inputStream = new StringReader(command);
+		
+		TicTacToeApp application = new TicTacToeApp(inputStream, printStream);
+		
+		application.run();
+		
+		String expected = 
+				"  A\n" +
+				"1 X\n";
+		
+		assertTrue(gameOutput().contains(expected));
+	}
+	
+	private String gameOutput() throws UnsupportedEncodingException {
+		return outputBaos.toString("UTF-8");
+	}
+	
+}
